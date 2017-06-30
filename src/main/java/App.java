ad161f3dc0c1f9a4461908eb73f77daea67f7503
wcchang1382 <wcc1213@gmail.com>
2017-06-30 16:39:41 -0700

    Edit App.java for output.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,33 @@
 import java.util.Scanner;
-import java.io.Console;
+// import java.io.Console;
 
 public class App {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("What kind of event are you planning?");
-    String eventname = sc.nextLine();
-    System.out.println("How many people are going?");
+    System.out.println("Welcome to the Epicodus Party Planner\nWhat kind of event are you planning?");
+    String eventname = sc.next();
+    
+    System.out.println("How many people are attending?");
     int guestcount = sc.nextInt();
-    System.out.println("What party food are you looking to serve your guests?");
-    String partyfood = sc.nextLine();
-    System.out.println("What beverages are you looking to serve your guests?");
-    String partydrinks = sc.nextLine();
-    System.out.println("What form of entertainment are you going to provide?");
-    String entertainment = sc.nextLine();
+    
+    System.out.println("For food: Appetizers, Full-Meal, or Desserts?");
+    String partyfood = sc.next();
+    
+    System.out.println("For beverages: Cocktails, Beers, or Juices");
+    String partydrinks = sc.next();
+    
+    System.out.println("For entertainment: Live Band, DJ, or None?");
+    String entertain = sc.next();
+    
+    System.out.println("Thank you for your booking. You are all set for your upcoming event: " + eventname);
+    System.out.println("The number of people that are attending is: " + guestcount);
+    System.out.println("For food, your guests will be served: " + partyfood);
+    System.out.println("For drinks, your guests will be served: " + partydrinks);
+    System.out.println("Your guests will be entertained by: " + entertain);
 
+    Event event = new Event(eventname, guestcount, partyfood, partydrinks, entertain);
+    int cost = event.computeCost();
+    System.out.println("Your total cost for this event will be: $" + cost);
   }
 }
